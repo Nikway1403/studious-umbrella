94c5cc2ff9842c8652578bfb98d489c948dac935
RisingLegend <babashov.nikita2@gmail.com>
2026-03-25 13:25:00 +0300

    chore: end Program.cs

--- services/authorization-service/src/Presentation/AuthService.GrpcApi/Program.cs
+++ services/authorization-service/src/Presentation/AuthService.GrpcApi/Program.cs
@@ -1,8 +1,15 @@
+using AuthService.GrpcApi.Controllers;
+using AuthService.GrpcApi.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddApplicationServices(builder.Configuration);
+
 builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
 
+app.MapGrpcService<AuthorizationGrpcService>();
 
 app.Run();
